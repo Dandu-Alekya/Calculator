66b08fb0423bbd118d4b24a45946dce2da26c386
Alekya Dandu <alekya.dandu@klh.edu.in>
2021-10-21 14:29:42 +0530

    functionality to call operand

--- Calculator.js
+++ Calculator.js
@@ -1,11 +1,11 @@
 let currentdisplay=' ';
-
+let firstnumber,secondnumber,result,operand,f=0;
 let current = document.querySelector('#screen');
 
 
 function add(a,b){
     console.log(parseFloat(a)+parseFloat(b));
-    return parseFloat(a)+parseFloat(b);
+    //return parseFloat(a)+parseFloat(b);
 }
 
 function sub(a,b){
@@ -29,13 +29,35 @@ function div(a,b){
 }*/
 
 function display(x){
-    current.textContent=x;
-    console.log(x);
+    //console.log(x==='=');
+    if((x==="+" || x==="-" || x==='*' || x==='/' || x==='^')){
+        console.log("inside operand");
+        operand=x;
+        current.textContent+=x;
+        console.log(x);
+        f=1;
+    }
+    else if(f===1 && x!=='='){
+        console.log("inside second");
+        current.textContent+=x;
+        secondnumber=current.textContent;
+        console.log(secondnumber);
+    }
+    else if(f===0 && x!=='='){
+        console.log("inside first");
+        current.textContent+=x;
+        firstnumber=current.textContent;
+        console.log(firstnumber);
+    }
+    else if(x==='='){
+        console.log("inside =");
+        operate(firstnumber,secondnumber,operand);
+    }
 }
 
 function operate(a,b,operator){
     switch(operator){
-        case '+' : return add(a,b); 
+        case '+' : add(a,b); break;
         case '-' : return sub(a,b); 
         case '*' : return mul(a,b); 
         case '/' : return div(a,b); 
