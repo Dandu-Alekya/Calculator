76e6c183f91e001554413be7a56fe637e052524b
Alekya Dandu <alekya.dandu@klh.edu.in>
2021-10-21 14:35:14 +0530

    added function to display final result

--- Calculator.js
+++ Calculator.js
@@ -1,11 +1,10 @@
-let currentdisplay=' ';
 let firstnumber,secondnumber,result,operand,f=0;
 let current = document.querySelector('#screen');
 
 
 function add(a,b){
     console.log(parseFloat(a)+parseFloat(b));
-    //return parseFloat(a)+parseFloat(b);
+    return parseFloat(a)+parseFloat(b);
 }
 
 function sub(a,b){
@@ -28,36 +27,39 @@ function div(a,b){
     return Math.power(a,b);
 }*/
 
+function displayresult(x){
+    current.textContent=x;
+}
 function display(x){
-    //console.log(x==='=');
     if((x==="+" || x==="-" || x==='*' || x==='/' || x==='^')){
-        console.log("inside operand");
+        //console.log("inside operand");
         operand=x;
         current.textContent+=x;
-        console.log(x);
+        //console.log(x);
         f=1;
     }
     else if(f===1 && x!=='='){
-        console.log("inside second");
+        //console.log("inside second");
         current.textContent+=x;
         secondnumber=current.textContent;
-        console.log(secondnumber);
+        //console.log(secondnumber);
     }
     else if(f===0 && x!=='='){
-        console.log("inside first");
+        //console.log("inside first");
         current.textContent+=x;
         firstnumber=current.textContent;
-        console.log(firstnumber);
+        //console.log(firstnumber);
     }
     else if(x==='='){
-        console.log("inside =");
-        operate(firstnumber,secondnumber,operand);
+        //console.log("inside =");
+        //operate(firstnumber,secondnumber,operand);
+        displayresult(operate(firstnumber,secondnumber,operand));
     }
 }
 
 function operate(a,b,operator){
     switch(operator){
-        case '+' : add(a,b); break;
+        case '+' : return add(a,b); 
         case '-' : return sub(a,b); 
         case '*' : return mul(a,b); 
         case '/' : return div(a,b); 
