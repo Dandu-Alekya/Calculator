30b8624b7fc80e8e81ad90c75b98d75a7fb32afc
Alekya Dandu <alekya.dandu@klh.edu.in>
2021-11-02 10:07:56 +0530

    Added AC/Clear button functionality

--- Calculator.js
+++ Calculator.js
@@ -5,16 +5,15 @@ const screen = document.querySelector('.display');
 const numbuttons = document.querySelectorAll('.num');
 const operators = document.querySelectorAll('.operator');
 let equals = document.querySelector('.equal');
+let clear = document.querySelector('.clear');
 numbuttons.forEach((num)=>{
     num.addEventListener('click', ()=>{
-        //console.log("hiii");
         displaynum(num.id);
     });
 });
 
 operators.forEach((op)=>{
     op.addEventListener('click', ()=>{
-        //console.log("hi");
         displayoperator(op.id);
     });
 });
@@ -27,8 +26,12 @@ equals.addEventListener('click',()=>{
     f=0;
 });
 
+clear.addEventListener('click',()=>{
+    cleardisplay();
+});
+
+
 function displaynum(number){
-    //console.log("hi num");
     current.textContent+=number;
     if(f===0){
         firstnumber+=number;
@@ -56,49 +59,11 @@ ans.setAttribute('id','result')
 function displayresult(x){
     ans.textContent=x;
     screen.append(ans);
-    //firstnumber=x;
-    //console.log(firstnumber);
 }
-/*
-function display(x){
-    if((x==="+" || x==="-" || x==='x' || x==='÷' || x==='^')){
-        console.log("inside operand");
-        operand=x;
-        current.textContent+=x;
-        console.log(x);
-        f=1;
-    }
-    else if(f===1 && x!=='='){
-        console.log("inside second");
-        current.textContent+=x;
-        secondnumber=current.textContent;
-        if(secondnumber===0){
-            displayresult("Cannot Divide by zero!");
-        }
-        console.log(secondnumber);
-    }
-    else if(f===0 && x!=='='){
-        console.log("inside first");
-        current.textContent+=x;
-        firstnumber=current.textContent;
-        console.log(firstnumber);
-    }
-    else if(x==='='){
-        //console.log("inside =");
-        //operate(firstnumber,secondnumber,operand);
-        console.log(parseFloat(firstnumber));
-        console.log(parseFloat(secondnumber));
-        displayresult(operate(firstnumber,secondnumber,operand));
-    }
-    else if(x=="AC"){
-        cleardisplay();
-    }
-}
-*/
 
 function cleardisplay(){
     firstnumber="";
-    operand="";
+    operator="";
     secondnumber="";
     current.textContent="";
     ans.remove();
@@ -112,13 +77,12 @@ function operate(a,b,operator){
         case '÷' : if(b===0) return null;
                     else return div(a,b); 
         case '^' : return power(a,b); 
-        default: console.log("Wrong operator");
+        default: return null //console.log("Wrong operator");
 
     }
 }
 
 function add(a,b){
-    //console.log(parseFloat(a)+parseFloat(b));
     return parseFloat(a)+parseFloat(b);
 }
 
